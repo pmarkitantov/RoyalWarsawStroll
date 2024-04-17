2461b62d44ca95df162016ef9b440166fe180cf2
Pavel Markitantov <markitantov@mail.com>
2024-04-17 09:26:49 +0200

    Initial Commit

---
+++ RoyalWarsawStroll/RoyalWarsawStrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoyalWarsawStrollApp.swift
+//  RoyalWarsawStroll
+//
+//  Created by Pavel Markitantov on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoyalWarsawStrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
