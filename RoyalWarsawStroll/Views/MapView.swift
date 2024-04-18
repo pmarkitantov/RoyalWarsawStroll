9edbfae26de2f5e979e60ed16bde387d2326a72b
Pavel Markitantov <pmarkitantov@gmail.com>
2024-04-18 12:06:54 +0200

    feat: add custom header with landmark name to MapView

--- RoyalWarsawStroll/Views/MapView.swift
+++ RoyalWarsawStroll/Views/MapView.swift
@@ -5,23 +5,52 @@
 //  Created by Pavel Markitantov on 17/04/2024.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct MapView: View {
-    
     @EnvironmentObject private var vm: LandmarkViewModel
-    
+
     var body: some View {
         ZStack {
             Map(position: .constant(.region($vm.mapRegion.wrappedValue)))
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
 
+                Spacer()
+            }
         }
     }
-    
-    
 }
 
+extension MapView {
+    private var header: some View {
+        VStack {
+            Text(vm.mapLocation.name)
+                .font(.title2)
+                .fontWeight(.bold)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .animation(.none, value: vm.mapLocation)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "arrow.down")
+                        .font(.headline)
+                        .padding()
+                        .rotationEffect(.degrees(vm.showLocationList ? 180 : 0))
+                }
+
+                .foregroundStyle(.primary)
+
+            if vm.showLocationList {
+            }
+        }
+        .background(.thickMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+}
 
 #Preview {
     MapView()
